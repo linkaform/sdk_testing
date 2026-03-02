6e7c006b0576fd41ab1c7b5e562d29a1af9d732c
Linkaform Develop Team <backend@linkaform.com>
2026-03-02 12:34:46 -0600

    pruebas de pases

--- accesos/passes/integration/test_pases.py
+++ accesos/passes/integration/test_pases.py
@@ -6,26 +6,26 @@ from urllib.parse import urlparse
 
 from lkf_modules.accesos.items.scripts.Accesos.accesos_testing import *
 
-def get_assets(accesos_obj, mock_location_areas):
+def get_assets(acceso_obj, mock_location_areas):
     logging.info('=== GETTING LOCATIONS ===')
-    response_location = accesos_obj.catalogos_pase_location()
+    response_location = acceso_obj.catalogos_pase_location()
     location_uno = response_location['ubicaciones_default'][0]
     len_locations = len(response_location['ubicaciones_default'])
     if len_locations > 1:
         location_dos = response_location['ubicaciones_default'][1]
     logging.info('=== GETTING LOCATIONS AREAS ===')
-    response_area_uno = accesos_obj.catalogos_pase_area(location_uno)
+    response_area_uno = acceso_obj.catalogos_pase_area(location_uno)
     if len_locations > 1:
-        response_area_dos = accesos_obj.catalogos_pase_area(location_dos)
+        response_area_dos = acceso_obj.catalogos_pase_area(location_dos)
     assert mock_location_areas[location_uno] == response_area_uno['areas_by_location']
     if len_locations > 1:
         assert mock_location_areas[location_dos] == response_area_dos['areas_by_location']
-    assets_access_pass = accesos_obj.assets_access_pass(location_uno)
+    assets_access_pass = acceso_obj.assets_access_pass(location_uno)
     assert 'Visita General' in assets_access_pass['Perfiles']
     assert 'Candidatos' in assets_access_pass['Perfiles']
     return response_area_uno
 
-def test_create_pase(accesos_obj, mock_pase, mock_location_areas):
+def test_create_pase(acceso_obj, mock_pase, mock_location_areas):
     """
     Crea un articulo concesionado con varios equipos. 
     
@@ -37,9 +37,9 @@ def test_create_pase(accesos_obj, mock_pase, mock_location_areas):
     """
     logging.info('================> Arranca TEST #1: ...')
     logging.info('================> Load assets:')
-    location = get_assets(accesos_obj, mock_location_areas)
-    accesos_obj.use_api = False
-    res = accesos_obj.create_access_pass(mock_pase)
+    location = get_assets(acceso_obj, mock_location_areas)
+    acceso_obj.use_api = False
+    res = acceso_obj.create_access_pass(mock_pase)
     print('res=',res)
     assert res['status_code'] == 201
     logging.info('================> Arranca TEST #1: Creando Articulo concesionado2...')
@@ -61,15 +61,15 @@ def check_catalog_pase_response(data):
     # breakpoint()
     # assert isinstance(pase['visita_a'][0]['nombre'], str)
 
-def completar_pase(accesos_obj, res, mock_pase_app_update):
+def completar_pase(acceso_obj, res, mock_pase_app_update):
     access_pass = mock_pase_app_update.get('access_pass')
     folio = res['json'].get('id')
     mock_pase_app_update['folio'] = folio
-    response = accesos_obj.update_pass(access_pass, folio)
+    response = acceso_obj.update_pass(access_pass, folio)
     assert response['status_code'] == 202
     return response
 
-def test_create_pase_fecha_fija(accesos_obj, mock_pase_fecha_fija):
+def test_create_pase_fecha_fija(acceso_obj, mock_pase_fecha_fija):
     """
     Crea un articulo concesionado con varios equipos. 
     
@@ -80,18 +80,18 @@ def test_create_pase_fecha_fija(accesos_obj, mock_pase_fecha_fija):
         4. Se finaliza el turno
     """
     logging.info('Test: test_create_pase_fecha_fija')
-    accesos_obj.use_api = False
+    acceso_obj.use_api = False
     access_pass = mock_pase_fecha_fija['access_pass']
-    res = accesos_obj.create_access_pass(access_pass)
+    res = acceso_obj.create_access_pass(access_pass)
     print('res=',res)
     assert res['status_code'] == 201
     qr_code = res.get('json',{}).get('id')
     assert len(qr_code) == 24
-    data = accesos_obj.catalagos_pase_no_jwt(qr_code)
+    data = acceso_obj.catalagos_pase_no_jwt(qr_code)
     print('response', data)
     check_catalog_pase_response(data)
 
-def test_create_pase_nueva_visita(accesos_obj, mock_pase_nueva_vista):
+def test_create_pase_nueva_visita(acceso_obj, mock_pase_nueva_vista):
     """
     Crea un articulo concesionado con varios equipos. 
     
@@ -102,24 +102,24 @@ def test_create_pase_nueva_visita(accesos_obj, mock_pase_nueva_vista):
         4. Se finaliza el turno
     """
     logging.info('================> Arranca TEST AUTOREGISRO SIN VISTA A#3: ...')
-    res = accesos_obj.create_access_pass(mock_pase_nueva_vista)
+    res = acceso_obj.create_access_pass(mock_pase_nueva_vista)
     print('res=',res)
     assert res['status_code'] == 201
 
-def test_create_pase_app(accesos_obj, mock_pase_app, mock_pase_app_update):
+def test_create_pase_app(acceso_obj, mock_pase_app, mock_pase_app_update):
     """
     Creacion de pase con datos desde app
     """
     def verify_completar_pase(res):
-        res = completar_pase(accesos_obj, res, mock_pase_app_update )
+        res = completar_pase(acceso_obj, res, mock_pase_app_update )
     logging.info('================> Arranca Crea pase desde app #4: ....')
-    res = accesos_obj.create_access_pass(mock_pase_app)
+    res = acceso_obj.create_access_pass(mock_pase_app)
     print('res=',res)
     assert res['status_code'] == 201
     verify_completar_pase(res)
 
   
-def test_create_pase_auto_registro(accesos_obj, mock_pase_auto_registro):
+def test_create_pase_auto_registro(acceso_obj, mock_pase_auto_registro):
     """
     Crea un articulo concesionado con varios equipos. 
     
@@ -130,11 +130,11 @@ def test_create_pase_auto_registro(accesos_obj, mock_pase_auto_registro):
         4. Se finaliza el turno
     """
     logging.info('================> Arranca TEST AUTOREGISRO #2: ...')
-    res = accesos_obj.create_access_pass(mock_pase_auto_registro)
+    res = acceso_obj.create_access_pass(mock_pase_auto_registro)
     print('res=',res)
     assert res['status_code'] == 201
   
-def test_create_pase_auto_registro_sin_vista(accesos_obj, \
+def test_create_pase_auto_registro_sin_vista(acceso_obj, \
     mock_pase_auto_registro_sin_vista_a, mock_completar_pase_usaurio_actual):
     """
     Crea un articulo concesionado con varios equipos. 
@@ -147,30 +147,30 @@ def test_create_pase_auto_registro_sin_vista(accesos_obj, \
     """
 
     def verify_answers_pase():
-        record = accesos_obj.get_record_by_id(record_id, select_columns=['answers'])
+        record = acceso_obj.get_record_by_id(record_id, select_columns=['answers'])
         answers = record['answers']
-        motivo = answers[accesos_obj.pase_entrada_fields['tema_cita']]
+        motivo = answers[acceso_obj.pase_entrada_fields['tema_cita']]
         assert mock_pase_auto_registro_sin_vista_a['motivo'] == motivo
 
     def verify_answers_visita():
         visita_a = mock_completar_pase_usaurio_actual['access_pass']['visita_a'][0]
-        record = accesos_obj.get_record_by_id(record_id, select_columns=['answers'])
+        record = acceso_obj.get_record_by_id(record_id, select_columns=['answers'])
         answers = record['answers']
-        visita_a_pase = answers[accesos_obj.pase_entrada_fields['visita_a']]
+        visita_a_pase = answers[acceso_obj.pase_entrada_fields['visita_a']]
         if visita_a == 'Usuario Actual':
             assert len(visita_a_pase) > 0
         else:
             vista_catalog = visita_a_pase[0]
-            vista = vista_catalog[accesos_obj.CONF_AREA_EMPLEADOS_CAT_OBJ_ID ]
-            assert visita_a == vista[accesos_obj.mf['nombre_empleado']]
+            vista = vista_catalog[acceso_obj.CONF_AREA_EMPLEADOS_CAT_OBJ_ID ]
+            assert visita_a == vista[acceso_obj.mf['nombre_empleado']]
 
     logging.info('> Arranca TEST AUTOREGISRO SIN VISTA A : ...')
-    res = accesos_obj.create_access_pass(mock_pase_auto_registro_sin_vista_a)
+    res = acceso_obj.create_access_pass(mock_pase_auto_registro_sin_vista_a)
     assert res['status_code'] == 201
     record_id = res['json']['id']
     verify_answers_pase()
     if res['status_code'] == 201:
-        completar_pase(accesos_obj, res, mock_completar_pase_usaurio_actual)
+        completar_pase(acceso_obj, res, mock_completar_pase_usaurio_actual)
         verify_answers_visita()
 
 
